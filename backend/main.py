260d01ac5635f22ffeeb138371cad071655d49b1
Lirca Mihai-Catalin <mihaicatalinlirca@gmail.com>
2026-07-25 20:08:20 +0300

    fix: added FRONTEND_URL for CORS

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,12 @@ origins: list[str] = [
     "http://127.0.0.1:5173",
 ]
 
+frontend_url = os.getenv("FRONTEND_URL")
+
+if frontend_url:
+    origins.append(frontend_url)
+    origins.append(frontend_url.rstrip("/"))
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
