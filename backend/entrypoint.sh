036749b34b103d1c135cb5774008804da75cb13b
Lirca Mihai-Catalin <mihaicatalinlirca@gmail.com>
2026-07-25 20:18:55 +0300

    migrations now run on startup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Running database migrations..."
+alembic upgrade head
 celery -A celery_stuff.celery_app:celery worker --loglevel=info --pool=threads &
 celery -A celery_stuff.celery_app:celery beat --loglevel=info &
 exec uvicorn main:app --host 0.0.0.0 --port 8000
