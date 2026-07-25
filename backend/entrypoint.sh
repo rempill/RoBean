de6be9fd99cacb44d5dc15fffb547b894a72e978
Lirca Mihai-Catalin <mihaicatalinlirca@gmail.com>
2026-07-25 19:37:54 +0300

    updated entrypoint.sh to run celery task on startup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+celery -A celery_stuff.celery_app:celery worker --loglevel=info --pool=threads &
+celery -A celery_stuff.celery_app:celery beat --loglevel=info &
 exec uvicorn main:app --host 0.0.0.0 --port 8000
